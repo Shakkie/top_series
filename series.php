2b93338088fb9ede257141a2dd387c9bb4a6e791
aylina <ikaaponce23@lhusurbil.eus>
2024-10-09 12:41:38 +0200

    commit "primer commit prueba"

---
+++ series.php
@@ -0,0 +1,42 @@
+<?php 
+
+class Series {
+    private $titulo;
+    private $ISAN; // 8 digitos
+    private $descripcion;
+    private $anio_estreno;
+    private $puntuacion; // 1 al 5; estrellas como la profe
+
+    public function __construct($titulo, $ISAN,$descripcion,$anio_estreno,$puntuacion)
+    {
+        $this->titulo = $titulo;
+        $this->ISAN = $ISAN;
+        $this->descripcion = $descripcion;
+        $this->anio_estreno = $anio_estreno;
+        $this->puntuacion = $puntuacion;
+
+    }
+
+    public function getTitulo() {return $this->titulo;}
+
+	public function getISAN() {return $this->ISAN;}
+
+	public function getDescripcion() {return $this->descripcion;}
+
+	public function getAnioEstreno() {return $this->anio_estreno;}
+
+	public function getPuntuacion() {return $this->puntuacion;}
+
+	public function setTitulo( $titulo): void {$this->titulo = $titulo;}
+
+	public function setISAN( $ISAN): void {$this->ISAN = $ISAN;}
+
+	public function setDescripcion( $descripcion): void {$this->descripcion = $descripcion;}
+
+	public function setAnioEstreno( $anio_estreno): void {$this->anio_estreno = $anio_estreno;}
+
+	public function setPuntuacion( $puntuacion): void {$this->puntuacion = $puntuacion;}
+
+	
+
+}
