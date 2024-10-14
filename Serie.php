5e0ddd6ed92ef49562fdf0fab5059c329bf78aa5
Ane <ikazaitegi23@lhusurbil.eus>
2024-10-14 10:02:58 +0200

    commit 'funcionesCRUD'

--- Serie.php
+++ Serie.php
@@ -1,50 +1,88 @@
-<?php 
-
-class Serie {
+<?php
+include_once 'ConexionBD.php';
+class Serie
+{
     private $titulo; // puede repetirse
     private $ISAN; // 8 digitos -> no se puede repetir
     private $descripcion;
     private $anio_estreno;
-    private $puntuacion; // 1 al 5; estrellas como la profe
 
-    public function __construct($titulo, $ISAN,$descripcion,$anio_estreno,$puntuacion)
+    public function __construct($ISAN, $titulo, $descripcion, $anio_estreno)
     {
         $this->titulo = $titulo;
-        $this->ISAN = $ISAN; 
+        $this->ISAN = $ISAN;
         $this->descripcion = $descripcion;
         $this->anio_estreno = $anio_estreno;
-        $this->puntuacion = $puntuacion;
-
     }
-// comentario hacer funciones separadas del grud, se llama a la funcion y hace la insercion de datos
-// la conexion es una clase estatica 
+    // comentario hacer funciones separadas del grud, se llama a la funcion y hace la insercion de datos
+    // la conexion es una clase estatica 
 
     //Getter and Setters
-    public function getTitulo() {return $this->titulo;}
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
 
-	public function getISAN() {return $this->ISAN;}
+    public function getISAN()
+    {
+        return $this->ISAN;
+    }
 
-	public function getDescripcion() {return $this->descripcion;}
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
 
-	public function getAnioEstreno() {return $this->anio_estreno;}
+    public function getAnioEstreno()
+    {
+        return $this->anio_estreno;
+    }
 
-	public function getPuntuacion() {return $this->puntuacion;}
+    public function setTitulo($titulo): void
+    {
+        $this->titulo = $titulo;
+    }
 
-	public function setTitulo( $titulo): void {$this->titulo = $titulo;}
+    public function setISAN($ISAN): void
+    {
+        $this->ISAN = $ISAN;
+    }
 
-	public function setISAN( $ISAN): void {$this->ISAN = $ISAN;}
+    public function setDescripcion($descripcion): void
+    {
+        $this->descripcion = $descripcion;
+    }
 
-	public function setDescripcion( $descripcion): void {$this->descripcion = $descripcion;}
-
-	public function setAnioEstreno( $anio_estreno): void {$this->anio_estreno = $anio_estreno;}
-
-	public function setPuntuacion( $puntuacion): void {$this->puntuacion = $puntuacion;}
+    public function setAnioEstreno($anio_estreno): void
+    {
+        $this->anio_estreno = $anio_estreno;
+    }
 
     // Funciones grud de series
+    public function getSeriesBD()
+    {
+        $con = ConexionBD::getConnection();
+        $sqlQuery = "select * from serie";
+        $result = $con->prepare($sqlQuery);
+        $result->execute();
+        $series = [];
+        $contador = 0;
+        while ($row = $result->fetch(PDO::FETCH_NUM)) {
+            $series[$contador] = new Serie($row[0], $row[1], $row[2], $row[3]);
+            $contador++;
+        }
+        return $series;
+    }
 
-    
-
-    
-	
-
+    public function addSerie($serie)
+    {
+        $con = ConexionBD::getConnection();
+        $sqlInsert = "insert into serie values ( ?, ?, ?, ?)";
+        $insert = $con->prepare($sqlInsert);
+        $insert->bindValue(1, $serie->getISAN());
+        $insert->bindValue(2, $serie->getNombre());
+        $insert->bindValue(3, $serie->getDescripcion());
+        $insert->bindValue(4, $serie->getAnioEstreno());
+        $insert->execute();
+    }
 }
--- Usuario.php
+++ Usuario.php
@@ -1,35 +1,86 @@
-<?php 
-class Usuario{
+<?php
+class Usuario
+{
     private $nombre;
     private $email;
     private $password;
     private $rol; // puede ser administrador o usuario comun
 
 
-    public function __construct($nombre,$email,$password,$rol){
-        $this->nombre=$nombre;
-        $this->email=$email;
-        $this->password=$password;
-        $this->rol=$rol;
+    public function __construct($nombre, $email, $password, $rol)
+    {
+        $this->nombre = $nombre;
+        $this->email = $email;
+        $this->password = $password;
+        $this->rol = $rol;
     }
 
     // getter y setters
-    public function getNombre() {return $this->nombre;}
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
 
-	public function getEmail() {return $this->email;}
+    public function getEmail()
+    {
+        return $this->email;
+    }
 
-	public function getPassword() {return $this->password;}
+    public function getPassword()
+    {
+        return $this->password;
+    }
 
-	public function getRol() {return $this->rol;}
+    public function getRol()
+    {
+        return $this->rol;
+    }
 
-	public function setNombre( $nombre): void {$this->nombre = $nombre;}
+    public function setNombre($nombre): void
+    {
+        $this->nombre = $nombre;
+    }
 
-	public function setEmail( $email): void {$this->email = $email;}
+    public function setEmail($email): void
+    {
+        $this->email = $email;
+    }
 
-	public function setPassword( $password): void {$this->password = $password;}
+    public function setPassword($password): void
+    {
+        $this->password = $password;
+    }
 
-	public function setRol( $rol): void {$this->rol = $rol;}
+    public function setRol($rol): void
+    {
+        $this->rol = $rol;
+    }
 
-	
-    
+    // Funciones CRUD
+    public function getUsuariosBD()
+    {
+        $con = ConexionBD::getConnection();
+        $sqlQuery = "select * from usuario";
+        $result = $con->prepare($sqlQuery);
+        $result->execute();
+        $usuarios = [];
+        $contador = 0;
+        while ($row = $result->fetch(PDO::FETCH_NUM)) {
+            $usuarios[$contador] = new Usuario($row[1], $row[2], $row[3], $row[4]);
+            $contador++;
+        }
+        return $usuarios;
+    }
+
+    public function addUser($usuario)
+    {
+        $con = ConexionBD::getConnection();
+        $sqlInsert = "insert into usuario (nombre, email, password, rol) values ( ?, ?, ?, ?)";
+        $insert = $con->prepare($sqlInsert);
+        $insert->bindValue(1, $usuario->getNombre());
+        $insert->bindValue(2, $usuario->getEmailç());
+        $insert->bindValue(3, $usuario->getPassword());
+        $insert->bindValue(4, $usuario->getRol());
+        $insert->execute();
+    }
 }
